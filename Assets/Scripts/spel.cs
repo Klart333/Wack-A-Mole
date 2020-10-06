e7c7336f2fa6bea5a7703e619cc6a6affe46eddd
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-10-06 15:39:21 +0200

    försökte fixa

--- Assets/Scripts/spel.cs
+++ Assets/Scripts/spel.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class spel : MonoBehaviour
+public class Spel : MonoBehaviour
 {
     public GameObject[] mulls;
     public Transform[] spawns;
     public bool[] full;
     public float timer;
     public GameObject nymull;
-    public GameObject helst;
     public float range;
     public Camera camera;
     public int score;
@@ -20,26 +19,28 @@ public class spel : MonoBehaviour
     public float poängtime = 5;
     public bool gepoäng;
     public Sprite[] numbers;
-    public Text skortext;
+    public Text scoreText;
     public AudioSource musik;
     public AudioSource ljud;
-
-    // Start is called before the first frame update
     void Start()
     {
+        camera = Camera.main;
+
         ljud = GetComponent<AudioSource>();
-        helst = new GameObject();
         loseScreen.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        helst.transform.position += new Vector3(1,1,0) * Time.deltaTime;
+
         x += Time.deltaTime;
-        skortext.text = (string)score.ToString();
         timer += Time.deltaTime;
-        if (timer > Random.Range(10, 50)) 
+
+        scoreText.text = score.ToString();
+
+
+
+        if (timer > Random.Range(2, 10)) 
         {
             print("skapa mullvad");
             timer = 0;
@@ -55,28 +56,23 @@ public class spel : MonoBehaviour
             x = x % 10;
             print("det funkar");
         }
-        if (helst.transform.position.y > 10)
-        {
-            helst.transform.position = new Vector3(transform.position.x, 0, 0);
-        }
+
         if (liv <= 0)
         {
-            musik.Stop();
-            ljud.PlayOneShot(ljud.clip);
-            Time.timeScale = 0;
-            print("haha du förlorade!!!!?!");
-            loseScreen.SetActive(true);
+            GameOver(); // Extrahera till metod
         }
+
         print("hundra: " + score % 100);
         print("tio: " + score % 10);
         print("ett: " + score % 1);
-        helst.transform.position += new Vector3(1, 1, 0) * Time.deltaTime;
-        if (Input.GetMouseButtonDown(0)) //Du klickar en knapp
+
+
+        if (Input.GetMouseButtonDown(0)) // Du klickar en knapp
         {
             var v3 = Input.mousePosition;
             v3.z = 10.0f;
             v3 = Camera.main.ScreenToWorldPoint(v3);
-            print(Vector3.Distance(nymull.transform.position, v3));
+
             for (int i = 0; i < spawns.Length; i++)
             {
                 if (Vector3.Distance(nymull.transform.position, v3) < range)
@@ -85,6 +81,7 @@ public class spel : MonoBehaviour
                     Destroy(nymull);
                     full[i] = false;
                     print("test");
+
                     if (gepoäng == true)
                     {
                         if (poängtime > 3)
@@ -119,5 +116,15 @@ public class spel : MonoBehaviour
         {
             poängtime -= Time.deltaTime;
         }
+    }
+
+    private void GameOver()
+    {
+        musik.Stop();
+        ljud.PlayOneShot(ljud.clip);
+        Time.timeScale = 0;
+        loseScreen.SetActive(true);
+
+        print("Game Over");
     }
 }
