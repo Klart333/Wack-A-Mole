c024967ccb8122c5adae8c9cf015ff577d854fd9
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-10-13 14:42:31 +0200

    buggy

--- Assets/Scripts/John/GunScript.cs
+++ Assets/Scripts/John/GunScript.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
-    
+    private void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 position = Vector3Extensions.ReplaceWith(mousePos, z: -5);
+
+        transform.position = position;
+
+    }
 }
