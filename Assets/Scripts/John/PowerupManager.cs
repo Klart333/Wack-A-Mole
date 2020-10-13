e114acd32395457dc34b175e9e575839d1d0cc67
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-10-13 13:29:55 +0200

    push

---
+++ Assets/Scripts/John/PowerupManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    public static PowerupManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/John/UI/UIActivateGun.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UIActivateGun : MonoBehaviour
+{
+    private TextMeshProUGUI activateGunText;
+    private Animator animator;
+
+    private bool gunActivatible;
+    private void Awake()
+    {
+        activateGunText = GetComponentInChildren<TextMeshProUGUI>();
+        animator = GetComponentInChildren<Animator>();
+    }
+    private void Start()
+    {
+        GameManager.Instance.OnSharkKilled += CheckIfGun;
+    }
+
+    private void Update()
+    {
+        if (gunActivatible && Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+
+    private void CheckIfGun(float timeToKill)
+    {
+        if (GameManager.Instance.hitSpree >= 20)
+        {
+            GunActivatible();
+        }
+        else
+        {
+            GunNotActivatible();
+        }
+    }
+
+    private void GunActivatible()
+    {
+        animator.ResetTrigger("Reset");
+        animator.SetTrigger("Breath");
+        
+        activateGunText.text = "Press Space To Activate GUN!";
+        gunActivatible = true;
+    }
+
+    private void GunNotActivatible()
+    {
+        animator.ResetTrigger("Breath");
+        animator.SetTrigger("Reset");
+
+        activateGunText.text = "";
+        gunActivatible = false;
+    }
+}
