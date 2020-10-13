8453e5d8e2fbf02663113bd3694c081457f9d1e9
Klart333 <56590003+Klart333@users.noreply.github.com>
2020-10-13 20:48:03 +0200

    gun

--- Assets/Scripts/John/UI/UIScoreScript.cs
+++ Assets/Scripts/John/UI/UIScoreScript.cs
@@ -30,18 +30,52 @@ public class UIScoreScript : MonoBehaviour
         int value = baseValueForSharks - Mathf.RoundToInt(sharkTimeToKill);
         value = Mathf.RoundToInt((float)value * (1 + GameManager.Instance.hitSpree / 10));
 
-        if (sharkTimeToKill < 0.75f) // DOUBLE (Cool effect)
+        if (CheckForGunHits(sharkTimeToKill, value) == true)
         {
-            value *= 2;
-
-            responseText.PrintDef("Double!");
-
-            object[] parms = new object[] {0.1f, 2}; 
-            Camera.main.GetComponent<CameraScript>().StartCoroutine("ScreenShake", parms); // Could add it to the event but calling it from here allows me to easier implement bigger screenshakes from gunshots and such
+            AddAndUpdateScore(value);
+            return;
         }
 
+
+        if (sharkTimeToKill < 0.75f) // DOUBLE (Cool effect)
+        {
+            value = MultiplyEffect(value, 2f, "Double!", 0.1f, 2);
+        }
+
+        AddAndUpdateScore(value);
+    }
+
+    private void AddAndUpdateScore(int value)
+    {
         score += value;
         UpdateScore();
     }
 
+    private bool CheckForGunHits(float sharkTimeToKill, int value)
+    {
+        if (PowerupManager.Instance.gunActive && sharkTimeToKill < 0.75f)
+        {
+            MultiplyEffect(value, 6f, "Sextuple!", 3, 0.4f);
+            return true;
+        }
+        else if (PowerupManager.Instance.gunActive)
+        {
+            MultiplyEffect(value, 3f, "Triple!", 2f, 0.1f);
+            return true;
+        }
+
+        return false;
+    }
+
+    private int MultiplyEffect(int value, float multiplicativeValue, string response, float screenShakeAmount, float screenShakeTimes)
+    {
+        value = Mathf.RoundToInt(value * multiplicativeValue);
+        responseText.PrintDef(response);
+
+        float[] parms = new float[] { screenShakeAmount, screenShakeTimes };
+
+        Camera.main.GetComponent<CameraScript>().StartCoroutine("ScreenShake", parms); // Could add it to the event but calling it from here allows me to easier implement bigger screenshakes from gunshots and such
+        
+        return value;
+    }
 }
