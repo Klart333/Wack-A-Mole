c8b29e2f78a83ba730b13c7847b4c98f2ad0a032
Erik Simon <70256847+IMakeGoodSalads@users.noreply.github.com>
2020-10-14 12:50:39 +0200

    a

---
+++ Assets/Scripts/Erik/HighscoreList.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreList : MonoBehaviour
+{
+    public Text highname;
+    public Text highscore;
+    public Text highnum;
+
+    List<string> names = new List<string>();
+    List<int> scores = new List<int>();
+
+    public void Addperson(string name, int score)
+    {
+        names.Add(name);
+        scores.Add(score);
+    }
+
+    public void Uppdate()
+    {
+        foreach (string name in names)
+        {
+            highname.text += name + "\n";
+        }
+
+        foreach (int score in scores)
+        {
+            highscore.text += score + "\n";
+        }
+    }
+
+    private void Start()
+    {
+        Addperson("aaa", 900);
+        Uppdate();
+    }
+}
