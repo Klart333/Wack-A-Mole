2549999a73378b9a909511eb5bae9a7ed5d2d338
Kygar0 <56590264+Kygar0@users.noreply.github.com>
2020-10-20 12:54:47 +0200

    ljud och annat skumt

---
+++ Assets/Scripts/Leon/ScorePanele.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScorePanele : MonoBehaviour
+{
+    TextMeshProUGUI scoreText;
+    int score;
+    private void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+    public void skrivPoeng()
+    {
+        scoreText.GetComponent<TextMeshProUGUI>().text = score;
+    }
+
+}
